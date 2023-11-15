9c11526f4c4ce685fca2973dd3921b355fc07bed
keerthisa <107065392+keerthisa@users.noreply.github.com>
2023-11-15 09:57:38 +0530

    Update DomainInfo.java

--- DomainInfo.java
+++ DomainInfo.java
@@ -1,70 +1,46 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.HashMap;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class DomainInfo {
-   public DomainInfo() {
-   }
 
-   public static void main(String[] var0) throws UnknownHostException, IOException {
-      if (var0.length != 1) {
-         System.out.println("Usage: java DomainInfo <domain>");
-      } else {
-         String var1 = var0[0];
-         Socket var2 = new Socket("whois.internic.net", 43);
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        if (args.length != 1) {
+            System.out.println("Usage: java DomainInfo ");
+            return;
+        }
 
-         try {
-            InputStream var3 = var2.getInputStream();
-            OutputStream var4 = var2.getOutputStream();
-            PrintWriter var5 = new PrintWriter(var4);
-            var5.println(var1);
-            var5.flush();
-            BufferedReader var6 = new BufferedReader(new InputStreamReader(var3));
-            HashMap<String, Integer> var8 = new HashMap<String, Integer>();
+        String domain = args[0];
 
-            while(true) {
-               String var7;
-               String[] var9;
-               if ((var7 = var6.readLine()) == null) {
-                  System.out.println("Domain: " + var1);
-                  System.out.println("Expiry Date: " + (String)var8.get("Expiration Date"));
-                  System.out.println("Subnets: ");
-                  var9 = ((String)var8.get("NetRange")).split(", ");
-                  int var10 = var9.length;
+        //server
+        Socket socket = new Socket("whois.internic.net", 43);
+        InputStream in = socket.getInputStream();
+        OutputStream out = socket.getOutputStream();
 
-                  for(int var11 = 0; var11 < var10; ++var11) {
-                     String var12 = var9[var11];
-                     System.out.println("- " + var12);
-                  }
+        // query
+        PrintWriter writer = new PrintWriter(out);
+        writer.println(domain);
+        writer.flush();
 
-                  System.out.println("Contact Details: ");
-                  System.out.println("- Registrant Name: " + (String)var8.get("Registrant Name"));
-                  System.out.println("- Registrant Organization: " + (String)var8.get("Registrant Organization"));
-                  System.out.println("- Registrant Email: " + (String)var8.get("Registrant Email"));
-                  break;
-               }
+        //  response
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String line;
+        Map<String, String> whoisData = new HashMap<>();
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(": ");
+            whoisData.put(parts[0].trim(), parts[1].trim());
+        }
 
-               var9 = var7.split(": ");
-               var8.put(var9[0].trim(), var9[1].trim());
-            }
-         } catch (Throwable var14) {
-            try {
-               var2.close();
-            } catch (Throwable var13) {
-               var14.addSuppressed(var13);
-            }
-
-            throw var14;
-         }
-
-         var2.close();
-      }
-   }
+        // Print information
+        System.out.println("Domain: " + domain);
+        System.out.println("Expiry Date: " + whoisData.get("Expiration Date"));
+        System.out.println("Subnets: ");
+        for (String subnet : whoisData.get("NetRange").split(", ")) {
+            System.out.println("- " + subnet);
+        }
+        System.out.println("Contact Details: ");
+        System.out.println("- Registrant Name: " + whoisData.get("Registrant Name"));
+        System.out.println("- Registrant Organization: " + whoisData.get("Registrant Organization"));
+        System.out.println("- Registrant Email: " + whoisData.get("Registrant Email"));
+    }
 }
